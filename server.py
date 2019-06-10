e9b1dc50dddd1f8ecd2b2ee7efccc9c3dffcb959
Roman Kosolapov <kosolapov.re@gmail.com>
2019-06-10 23:11:45 +0300

    прошел урок с вынесением разметки в отдельный файл. Цель такого приема - разделить работу над логикой и визуальным представлением.

--- server.py
+++ server.py
@@ -1,25 +1,13 @@
-from flask import Flask 
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    page_title = 'Прогноз погоды'
     weather = weather_by_city('Moscow,Russia')
-    if weather:
-        weather_text =  f"Погода: {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-        weather_text = 'Сервис погоды временно недоступен'
-    return f'''
-    <html>
-        <head>
-            <title>Прогноз погоды</title>
-        </head>
-        <body>
-            <h1>{weather_text}</h1>
-        </body>
-    </html>
-    '''
+    return render_template('index.html', page_title=page_title, weather=weather)
 
 
 if __name__ == "__main__":
