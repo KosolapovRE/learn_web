02d0e95ec59ec92db9f19e26cf0ad770a08be6ef
Roman Kosolapov <kosolapov.re@gmail.com>
2019-06-11 09:09:33 +0300

    По видеоуроку сделали возможность забирать новости с сайта.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+
+from python_org_news import get_python_news
 from weather import weather_by_city
 
 app = Flask(__name__)
@@ -7,7 +9,8 @@ app = Flask(__name__)
 def hello():
     page_title = 'Новости Python'
     weather = weather_by_city('Moscow,Russia')
-    return render_template('index.html', page_title=page_title, weather=weather)
+    news_list = get_python_news()
+    return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
 
 
 if __name__ == "__main__":
