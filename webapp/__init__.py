99cafa24e10f44f4570f9e504cceeb4b72bfa417
Roman Kosolapov <kosolapov.re@gmail.com>
2019-06-15 12:33:37 +0300

    добавил ретурн.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -12,5 +12,6 @@ def create_app():
         weather = weather_by_city('Moscow,Russia')
         news_list = get_python_news()
         return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
-
+    
+    return app
 
