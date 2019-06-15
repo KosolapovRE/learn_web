92255273c58deea622fd505ba52b44f86e61ada4
Roman Kosolapov <kosolapov.re@gmail.com>
2019-06-15 13:10:34 +0300

    не помню, что сделал'

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-from python_org_news import get_python_news
-from weather import weather_by_city
+from webapp.python_org_news import get_python_news
+from webapp.weather import weather_by_city
 
 def create_app():
     app = Flask(__name__)    
