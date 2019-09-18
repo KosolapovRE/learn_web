2937f0263ec2cfd3acec2e79a77cb065528042f9
Roman Kosolapov <kosolapov.re@gmail.com>
2019-09-18 09:36:40 +0300

    Добавлена модель Comment. Изменения записаны в базу. Создан тестовый комментарий к новости

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
 from webapp.db import db
 
 class News(db.Model):
@@ -9,3 +11,23 @@ class News(db.Model):
 
     def __repr__(self):
         return '<News {} {} {}>'.format(self.title, self.url, self.published)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+    news = relationship('News', backref='comments')
+    user = relationship('User', backref='comments')
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
